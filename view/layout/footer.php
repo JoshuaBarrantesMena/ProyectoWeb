5cf5af51edebf3608326bb758cb34a7b9459f0f6
JoshuaBarrantesMena <joshua.barrantes.mena@est.una.ac.cr>
2025-04-06 00:21:31 -0600

    Initial commit

---
+++ view/layout/footer.php
@@ -0,0 +1,20 @@
+<footer>
+    <div class="footer_content">
+        <div class="footer_left">
+            <a href=""><img src="view/img/Logo.png" alt="Logo"></a>
+        </div>
+        <div class="footer_center">
+            <p>&copy; Viajeros del Sul y Empresas Coordinadas, S.A. de C.V. México, 2025. Todos los derechos reservados.</p>
+        </div>
+        <div class="footer_right">
+            <ul class="footer_icons">
+                <li><a href=""><img src="view/img/IconCall.png" alt="Phone"></a></li>
+                <li><a href=""><img src="view/img/IconLocation.png" alt="Location"></a></li>
+                <li><a href=""><img src="view/img/IconWhatsapp.png" alt="Whatsapp"></a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
